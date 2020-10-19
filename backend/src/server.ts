2dd2a543632e770de8c4875aa49f57e738669092
ppegoretti <ppegoretti@hotmail.com>
2020-10-19 13:10:37 -0300

    Start: Inicio do async-erros

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,18 @@
 import express, { json } from 'express';
 import path from 'path';
+import 'express-async-errors';
+
 import routes from './routes';
 
 import './database/connection';
+import errorHandler from './erros/handler';
 const app = express();
 
 app.use(json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+
+app.use(errorHandler);
 
 // app.post('/users/:id', (request, response) => {
 //   console.log(request.query);
