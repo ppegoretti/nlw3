f3f75d55e1e4a806333de4ac9359428f9c325b9d
ppegoretti <ppegoretti@hotmail.com>
2020-11-06 13:07:49 -0300

    Tela inicial

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -3,7 +3,13 @@ import styles from '../styles/pages/landing.module.css';
 const Home: React.FC = () => {
   return (
     <div className={styles.page_landing}>
-      <h1>Patrick Fernando Pegoretti</h1>;
+      <div className='content-wrapper'>
+        <img src='/logo.svg' alt='Happy' />
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+      </div>
     </div>
   );
 };
